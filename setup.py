610013b8a1670f74b52f90812b2544d316b962ab
sq8kfh <sq8kfh@gmail.com>
2021-06-03 20:11:47 +0200

    Adjust setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,7 @@
 from setuptools import setup, find_packages
-from codecs import open
-from os import path
 
-here = path.abspath(path.dirname(__file__))
-
-with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
-    long_description = f.read()
+with open("README.rst", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
     name='hamutils',
